1cb66566ee187f975a2d6b4c118ab3fd85adc30a
Dhruvesh Sheladiya <dhruveshsheladiya@gmail.com>
2018-07-30 09:45:48 -0400

    Update queue.go

--- queue.go
+++ queue.go
@@ -4,6 +4,16 @@ const (
 	dequed = "dequed"
 )
 
+// Queue represents a named queue.
+type Queue string
+
+// Task represents a task in a queue.
+type Task struct {
+	ID    uint64 `json:"id"`    // Task's globally unique ID assigned by taskq.
+	Queue Queue  `json:"queue"` // Task's queue.
+	Body  []byte `json:"body"`  // Task's client data.
+}
+
 func (h EnQHandler) Rcv(msg beehive.Msg, ctx beehive.RcvContext) error {
 	// TODO: assign a unique ID.
 	enq := msg.Data().(Enque)
